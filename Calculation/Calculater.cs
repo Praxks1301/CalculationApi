c744b34baf6dcd4ab5d59dac57579d1e60a2b237
Dmitry Pakhomov <wypynobept@yandex.ru>
2020-08-31 22:59:46 +0300

    apiv2

---
+++ Calculation/Calculater.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    public class Calculater
+    {
+        public string First { get; set; }
+
+        public string Second { get; set; }
+
+        public string Znak { get; set; }
+    }
+}
--- Calculation/Controllers/Controller.cs
+++ Calculation/Controllers/Controller.cs
@@ -11,6 +11,59 @@ namespace Calculation.Controllers
     [Route("[controller]")]
     public class Controller : ControllerBase
     {
-        
+        [HttpPost]
+        public string Result(string First, string Second, string Znak)
+        {
+
+            if (!Double.TryParse(First, out double result))
+            {
+                return "Неверный первый входной параметр";
+
+            }
+            else
+            {
+                if (!Double.TryParse(Second, out result))
+                {
+                    return "Неверный второй входной параметр";
+                }
+                else
+                {
+                }
+                if (Znak == "Inc")
+                {
+                    return Convert.ToString(Convert.ToDouble(First) + Convert.ToDouble(Second));
+                }
+                else
+                {
+                    if (Znak == "Dec")
+                    {
+                        return Convert.ToString(Convert.ToDouble(First) - Convert.ToDouble(Second));
+                    }
+                    else
+                    {
+                        if (Znak == "Ymno")
+                        {
+                            return Convert.ToString(Convert.ToDouble(First) * Convert.ToDouble(Second));
+                        }
+                        else
+                        {
+                            if (Znak == "Del")
+                            {
+                                if (Convert.ToDouble(Second) == 0)
+                                { return "Деление на нуль"; }
+                                else
+                                {
+                                    return Convert.ToString(Convert.ToDouble(First) / Convert.ToDouble(Second));
+                                }
+                            }
+                            else
+                            {
+                                return "Ошибка математического знака";
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
